afe42009596d5b77cc25a2c11057540859831eb2
FANG <513069595@qq.com>
2017-11-19 21:51:48 +0800

    部署修改配置

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0!+@=q##qp7(@_uq9wy@f^*8m*^-j3e3oymbxx)$igw^4j1-4o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -109,6 +109,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
